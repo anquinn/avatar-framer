0313de95bac88c48bad285038accffb8558c2ae9
Alexander Taylor <ajxtaylor@gmail.com>
2024-01-11 23:14:43 +0000

    Set canvass image from relative path to support serving app from under a base path

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -171,6 +171,11 @@ function resizeCanvas() {
 }
 
 function changeFrame(canvas, image) {
+  // Use relative paths to properly locate images when this site is being served
+  // under a base path such as "example.org/image-framer"
+  if (image.startsWith('/')) {
+    image = image.substring(1);
+  }
   canvas.setOverlayImage(image, function() {
         canvas.overlayImage.scaleToWidth(canvas.getWidth())
         canvas.renderAll()
